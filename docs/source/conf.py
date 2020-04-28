a85b4d62b0f076cbdc539028de9162a97ff30403
Roma Koshel <roma.koshel@gmail.com>
2020-04-28 23:12:38 +0300

    wip. docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -60,7 +60,8 @@ html_theme = 'sphinx_rtd_theme'
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
-
 autodoc_default_options = {
     'special-members': '__init__',
 }
+
+master_doc = 'index'
