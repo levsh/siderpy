f31883d6be5e94f2dfee211b9ef4b86e4124f94d
Roma Koshel <roma.koshel@gmail.com>
2020-04-29 20:12:34 +0300

    fix

--- tests/tests.py
+++ tests/tests.py
@@ -18,7 +18,7 @@ pytestmark = pytest.mark.asyncio
 
 
 @pytest.fixture
-def event_loop(scope='function'):
+def event_loop():
     loop = asyncio.new_event_loop()
     asyncio.set_event_loop(loop)
     try:
